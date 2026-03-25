e91d3e9ae0ddd58d146f92ee95c414c8de741c81
FLAVIO GOMES SOARES <FlavioGomesSoares2007@gmail.com>
2026-03-25 06:47:44 -0300

    falha de import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home/Home";
-import { Projects } from "./pages/projects/Projects";
 import { AboutMe } from "./pages/about me/AboutMe";
 
 function App() {
